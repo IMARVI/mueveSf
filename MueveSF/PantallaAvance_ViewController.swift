fa14307054ca0f199613ed07f7287004bc211149
IMARVI <isaiasmarvi@gmail.com>
2017-10-29 19:08:14 -0600

    Initial Commit

---
+++ MueveSF/PantallaAvance_ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  PantallaAvance_ViewController.swift
+//  MueveSF
+//
+//  Created by admin on 12/09/17.
+//  Copyright © 2017 marvi. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class PantallaAvance_ViewController: UIViewController, CLLocationManagerDelegate{
+    
+    @IBOutlet weak var Map: MKMapView!
+    @IBOutlet weak var mapa: UIImageView!
+    @IBOutlet weak var timer: UILabel!
+    
+    var t = Timer()
+    var counter = 0
+    var x = Timer()
+    //@IBAction func stop(_ sender: UIButton) {
+        //t.invalidate()
+        //counter = 0
+      //  timer.text = String("00:00:00")
+        
+    //}
+    @objc let locationManager = CLLocationManager()
+    let regionRadius: CLLocationDistance = 1000
+    var location = CLLocation(latitude: 21.282778, longitude: -157.829444)
+    @IBAction func showM(_ sender: UIButton) {
+        if Map.isHidden {
+            sender .setTitle("Ocultar Mapa", for: UIControlState .normal)
+            Map.isHidden = false
+        }
+        else{
+            sender .setTitle("Mostrar Mapa", for: UIControlState .normal)
+            Map.isHidden = true
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        self.Map.showsUserLocation = true;
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        timer.text = String("00:00:00")
+        t = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(PantallaAvance_ViewController.updateCounter), userInfo: nil, repeats: true)
+        Map.isHidden = false
+        x = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(getter: PantallaAvance_ViewController.locationManager), userInfo: nil, repeats: true)
+        // Do any additional setup after loading the view.
+     }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        location = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius,regionRadius)
+        Map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func updateCounter() {
+        counter += 1
+        let hrs = counter/36000
+        let min = (counter%3600)/60
+        let seg = (counter%3600)%60
+        var txt = ""
+        if min<10 {
+            txt = "0\(hrs):0\(min):\(seg)"
+        }else{
+        txt = "0\(hrs):\(min):\(seg)"
+        }
+        timer.text = String(txt)
+    }
+}
